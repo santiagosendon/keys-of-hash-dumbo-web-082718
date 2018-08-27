380a80384aba55c406093391b378b020cc6421de
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-27 01:16:06 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,11 @@
 class Hash
   def keys_of(arguments)
- animals = []
-		self.each { |animal, place|
-			if arguments.include?(place)
-				animals << animal
-			end
-		}
-		return animals
-end
-end
+ listOfKeys=[]
+ argument.each do |arg|
+    map do |key,value|
+     if arg == value
+   listOfKeys<< key
+      end
+     end
+    end
+listOfKeys
